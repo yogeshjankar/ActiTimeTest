0d8806b7c3e51335b976262332dac8ff64e64b3b
Yogesh Jankar <jankaryogesh2972@gmail.com>
2022-06-16 08:25:00 +0530

    1

--- src/testpackage/TestActiTimeInvalidLogin.java
+++ src/testpackage/TestActiTimeInvalidLogin.java
@@ -10,31 +10,31 @@ import generic.Flib;
 import pagepackage.LoginPage;
 
 public class TestActiTimeInvalidLogin extends BaseTest {
-	
-	
-	
+
+
+
 	@Test
 	public void invalidLogin() throws EncryptedDocumentException, IOException, InterruptedException
 	{
-		
+
 		BaseTest bt = new BaseTest();
 		bt.setUp();
-	
+
 		Flib flib = new Flib();
 		int rc = flib.getRowCount(EXCEL_PATH, "invalidcreds");
-		
+
 		LoginPage lp = new LoginPage(driver);
-		
+
 		for(int i=1;i<=rc;i++)
 		{
-		String invalidUsername = flib.readExcelData(EXCEL_PATH, "invalidcreds", i, 0);
-		String invalidPassword = flib.readExcelData(EXCEL_PATH, "invalidcreds", i, 1);
-		
-		lp.actiTimeInvalidLogin(invalidUsername, invalidPassword);
-		
-		
-		
-		
+			String invalidUsername = flib.readExcelData(EXCEL_PATH, "invalidcreds", i, 0);
+			String invalidPassword = flib.readExcelData(EXCEL_PATH, "invalidcreds", i, 1);
+
+			lp.actiTimeInvalidLogin(invalidUsername, invalidPassword);
+
+
+
+
 		}
 		bt.tearDown();
 	}
